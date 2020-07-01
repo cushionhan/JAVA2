bc70466d2b3a405cf08199d0bd43899378e77106
soldesk <soldesk@DESKTOP-6NHGCE4>
2020-07-01 17:57:20 +0900

    20200701 JAVA2-2

---
+++ JAVA2/src/pk22/BoxingUnboxingExample.java
@@ -0,0 +1,20 @@
+package pk22;
+
+public class BoxingUnboxingExample {
+
+	public static void main(String[] args) {
+		//Boxing 기본타입=>객체
+		Integer obj1=new Integer(100);
+		Integer obj2=new Integer("200");
+		//Integer obj3=new Integer.valueOf("300");
+		
+		//Unboxing 객체를 기본타입으로 
+		int value1=obj1.intValue();
+		int value2=obj2.intValue();
+		
+		System.out.println(value1);
+		System.out.println(value2);
+		
+	}
+
+}
